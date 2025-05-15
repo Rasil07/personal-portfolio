10a6ff085fefeeb12879c6677809983b3ad3d247
Rasil07 <rasilgrt@gmail.com>
2025-05-16 00:11:48 +1000

    tweaked content for testing CI CD with vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ export default function Home() {
       <Header />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <div className="font-display text-4xl text-primary-500"> Heading </div>
-        <button className="glass-effect px-4 py-2 rounded-lg cursor-pointer ">
-          Rasil
-        </button>
+        <h1>Starting my portfolio website</h1>
       </div>
     </main>
   );
