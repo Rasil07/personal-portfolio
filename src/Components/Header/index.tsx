b0125e3edb9e4b0c171bfe670db5033f3d01fb6f
Rasil07 <rasilgrt@gmail.com>
2025-05-19 23:47:44 +1000

    commented unused vars

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -90,8 +90,8 @@ const Header = () => {
 
   if (!mounted) return null;
 
-  const currentTheme = theme === "system" ? systemTheme : theme;
-  const isDark = currentTheme === "dark";
+  // const currentTheme = theme === "system" ? systemTheme : theme;
+  // const isDark = currentTheme === "dark";
 
   return (
     <div className="flex w-full py-1.5 absolute top-0 left-0 items-center px-7">
