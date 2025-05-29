f1584865ffae11d158028f0a8addee5aa9871189
Rasil07 <rasilgrt@gmail.com>
2025-05-29 11:02:59 +1000

    added small about summary section in landing page

--- src/Components/LandingSection.tsx
+++ src/Components/LandingSection.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "motion/react";
 import { useEffect, useState } from "react";
 
 export default function LandingSection() {
@@ -34,7 +35,53 @@ export default function LandingSection() {
           </div>
         </section>
       </section>
-
+      <section className="w-full mx-auto px-4 py-8 flex items-center">
+        <div className="text-center w-3/5">
+          <p className="text-xl md:text-3xl lg:text-2xl text-zinc-700 dark:text-zinc-300 leading-relaxed font-serif">
+            Turning complex ideas into elegant solutions through code. I craft
+            seamless digital experiences with modern tech, bringing creativity
+            and precision to every project.
+          </p>
+        </div>
+        <div className="flex flex-1 justify-center items-center">
+          <motion.a
+            href="/about"
+            className="w-20 h-20 rounded-full bg-zinc-800 dark:bg-zinc-100 text-zinc-100 dark:text-zinc-800 flex items-center justify-center"
+            initial={{ y: -100, x: 0, opacity: 0 }}
+            whileInView={{
+              y: 0,
+              x: 0,
+              opacity: 1,
+              // rotate: scrollX * 0.022,
+              transition: {
+                y: {
+                  type: "spring",
+                  bounce: 0.2,
+                  duration: 1.5,
+                },
+                x: {
+                  type: "spring",
+                  bounce: 0.3,
+                  duration: 2,
+                  delay: 1.5,
+                },
+                opacity: {
+                  duration: 0.5,
+                },
+              },
+            }}
+            style={{
+              transformOrigin: "center",
+            }}
+            whileHover={{
+              scale: 1.1,
+              transition: { duration: 0.2 },
+            }}
+          >
+            <span className="text-lg font-medium">Learn More</span>
+          </motion.a>
+        </div>
+      </section>
       <style jsx global>{`
         @keyframes scroll {
           0% {
