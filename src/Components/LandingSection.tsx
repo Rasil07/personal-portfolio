a34f830ab21c0dfac559d087ba3134248f555d68
Rasil07 <rasilgrt@gmail.com>
2025-06-20 23:09:50 +1000

    tweaked landing section

--- src/Components/LandingSection.tsx
+++ src/Components/LandingSection.tsx
@@ -20,7 +20,48 @@ export default function LandingSection() {
       <div className="h-10 bg-zinc-300 dark:bg-zinc-600"></div>
       <section className="min-h-screen pt-0 bg-zinc-300 dark:bg-zinc-600">
         <section className="relative min-h-screen pt-24 pb-12 flex items-center justify-center px-4 sm:px-6 lg:px-8 bg-[url('../images/Subject1.png')] dark:bg-[url('../images/Subject2.png')] bg-center bg-no-repeat bg-contain">
-          <div className="absolute bottom-16 w-full overflow-hidden bg-transparent">
+          <div className="absolute top-1/3 -translate-y-1/3 z-40 right-10 flex justify-center items-center ml-auto w-50 h-40">
+            <svg
+              className="w-8 h-8 text-white mr-2 -rotate-45 absolute top-[20px] left-[-80px]"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.5}
+                d="M17 13l-5 5m0 0l-5-5m5 5V6"
+              />
+            </svg>
+            <p className="text-4xl text-white font-sans flex-1 tracking-widest">
+              Full Stack Software Engineer
+            </p>
+          </div>
+
+          <div className="absolute top-1/2 -translate-y-1/2 z-40 left-0 flex items-center py-2 w-fit bg-slate-700 pr-2 pl-4 rounded-tr-full rounded-br-full">
+            <div className="flex flex-col mr-3">
+              <p className="text-xl text-white">Located in Sydney,</p>
+              <p className="text-2xl text-white">Australia</p>
+            </div>
+            <svg
+              className="w-8 h-8 text-white transform hover:scale-110 transition-transform duration-300 ease-in-out"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1}
+                d="M12 21s-8-4.5-8-11.8A8 8 0 0112 1a8 8 0 018 8.2c0 7.3-8 11.8-8 11.8z"
+              />
+              <circle cx="12" cy="9" r="4" strokeWidth={0.7} />
+            </svg>
+          </div>
+          <div className="absolute  bottom-16 w-full overflow-hidden bg-transparent">
             <div className="whitespace-nowrap lg:text-[180px] sm:text-4xl tracking-[24px] pointer-events-none text-zinc-50 dark:text-zinc-200 animate-scroll">
               <span
                 className="inline-block w-full text-center"
@@ -137,6 +178,9 @@ export default function LandingSection() {
           </motion.a>
         </div>
       </section>
+
+      {/* Skills section */}
+
       <style jsx global>{`
         @keyframes scroll {
           0% {
